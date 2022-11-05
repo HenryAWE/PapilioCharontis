8c47ab96ba671d2ad4f2bb881b453e6f7fe3d255
AWE Henry <mouth11or12@outlook.com>
2022-11-05 11:59:24 +0800

    version 0.1.0

--- include/papilio/papilio.hpp
+++ include/papilio/papilio.hpp
@@ -1,5 +1,16 @@
 #pragma once
 
+#include <tuple>
 #include "core.hpp"
 #include "script.hpp"
 #include "format.hpp"
+
+
+namespace papilio
+{
+    inline constexpr int version_major = 0;
+    inline constexpr int version_minor = 1;
+    inline constexpr int version_patch = 0;
+
+    std::tuple<int, int, int> get_version() noexcept;
+}
--- src/papilio.cpp
+++ src/papilio.cpp
@@ -1,1 +1,14 @@
 #include <papilio/papilio.hpp>
+
+
+namespace papilio
+{
+    std::tuple<int, int, int> get_version() noexcept
+    {
+        return std::make_tuple(
+            version_major,
+            version_minor,
+            version_patch
+        );
+    }
+}
