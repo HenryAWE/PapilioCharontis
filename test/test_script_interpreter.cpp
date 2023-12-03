71fbb62eee5af82e6e99db49b01acbbf6a4a7c50
AWE Henry <mouth11or12@outlook.com>
2023-12-03 17:47:48 +0800

    remove old script interpreter

---
+++ test/test_script_interpreter.cpp
@@ -0,0 +1,15 @@
+#include <gtest/gtest.h>
+#include <papilio/script.hpp>
+
+
+TEST(interpreter, constructor)
+{
+    using namespace papilio;
+    using namespace script;
+}
+
+int main(int argc, char* argv[])
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
