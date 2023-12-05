fd61d2b99446c64b096654e72910a36985f449af
AWE Henry <mouth11or12@outlook.com>
2023-12-05 22:14:31 +0800

    update unit test

--- test/test_script_interpreter.cpp
+++ test/test_script_interpreter.cpp
@@ -2,135 +2,90 @@
 #include <papilio/script.hpp>
 
 
+namespace test_script_interpreter
+{
+    template <typename... Args>
+    auto test_access(std::string_view fmt, Args&&... args)
+    {
+        using namespace papilio;
+        auto fmt_args = PAPILIO_NS make_format_args(std::forward<Args>(args)...);
+
+        format_parse_context parse_ctx(fmt, fmt_args);
+        parse_ctx.advance_to(parse_ctx.begin() + 1); // skip '{'
+
+        script::interpreter intp;
+        auto [result, it] = intp.access(parse_ctx);
+
+        EXPECT_NE(it, parse_ctx.end());
+        EXPECT_TRUE(*it == U'}' || *it == U':');
+
+        return std::move(result);
+    }
+}
+
 TEST(interpreter, access)
 {
     using namespace papilio;
     using namespace script;
+    using namespace test_script_interpreter;
 
     {
-        static_format_args<1, 0> args(182375);
-        format_parse_context parse_ctx("{}", args);
-        parse_ctx.advance_to(parse_ctx.begin() + 1);
-
-        interpreter intp;
-
-        auto [arg, it] = intp.access(parse_ctx);
-
-        ASSERT_NE(it, parse_ctx.end());
-        EXPECT_EQ(*it, U'}');
+        format_arg arg = test_access("{}", 182375);
 
         ASSERT_TRUE(arg.holds<int>());
         EXPECT_EQ(get<int>(arg), 182375);
     }
 
     {
-        static_format_args<1, 0> args(182375);
-        format_parse_context parse_ctx("{:}", args);
-        parse_ctx.advance_to(parse_ctx.begin() + 1);
-
-        interpreter intp;
-
-        auto [arg, it] = intp.access(parse_ctx);
-
-        ASSERT_NE(it, parse_ctx.end());
-        EXPECT_EQ(*it, U':');
+        format_arg arg = test_access("{:}", 182375);
 
         ASSERT_TRUE(arg.holds<int>());
         EXPECT_EQ(get<int>(arg), 182375);
     }
 
     {
-        static_format_args<2, 0> args(182375, 182376);
-        format_parse_context parse_ctx("{1}", args);
-        parse_ctx.advance_to(parse_ctx.begin() + 1);
-
-        interpreter intp;
-
-        auto [arg, it] = intp.access(parse_ctx);
-
-        ASSERT_NE(it, parse_ctx.end());
-        EXPECT_EQ(*it, U'}');
+        format_arg arg = test_access("{1}", 182375, 182376);
 
         ASSERT_TRUE(arg.holds<int>());
         EXPECT_EQ(get<int>(arg), 182376);
     }
 
     {
-        static_format_args<0, 1> args("scene"_a = 182375);
-        format_parse_context parse_ctx("{scene}", args);
-        parse_ctx.advance_to(parse_ctx.begin() + 1);
-
-        interpreter intp;
-
-        auto [arg, it] = intp.access(parse_ctx);
-
-        ASSERT_NE(it, parse_ctx.end());
-        EXPECT_EQ(*it, U'}');
+        format_arg arg = test_access("{scene}", "scene"_a = 182375);
 
         ASSERT_TRUE(arg.holds<int>());
         EXPECT_EQ(get<int>(arg), 182375);
     }
 
     {
-        auto helper = [](std::string_view fmt) -> format_arg
-        {
-            static_format_args<0, 1> args("name"_a = "Hu Tao");
-            format_parse_context parse_ctx(fmt, args);
-            parse_ctx.advance_to(parse_ctx.begin() + 1);
+        format_arg arg = test_access("{}", "hello");
 
-            interpreter intp;
+        ASSERT_TRUE(arg.holds<utf::string_container>());
+        EXPECT_EQ(get<utf::string_container>(arg), "hello");
+    }
 
-            auto [arg, it] = intp.access(parse_ctx);
+    {
+        auto helper = [](std::string_view fmt) { return test_access(fmt, "name"_a = "Hu Tao"); };
 
-            EXPECT_NE(it, parse_ctx.end());
-            EXPECT_EQ(*it, U'}');
+        EXPECT_EQ(helper("{name.length}").as_variable(), 6);
 
-            return std::move(arg);
-        };
+        EXPECT_EQ(helper("{name[0]}").as_variable(), "H");
 
-        {
-            format_arg arg = helper("{name.length}");
-            EXPECT_EQ(arg.as_variable(), 6);
-        }
+        EXPECT_EQ(helper("{name[-1]}").as_variable(), "o");
 
-        {
-            format_arg arg = helper("{name[0]}");
-            EXPECT_EQ(arg.as_variable(), "H");
-        }
+        EXPECT_EQ(helper("{name[3:]}").as_variable(), "Tao");
 
-        {
-            format_arg arg = helper("{name[-1]}");
-            EXPECT_EQ(arg.as_variable(), "o");
-        }
+        EXPECT_EQ(helper("{name[-3:]}").as_variable(), "Tao");
 
-        {
-            format_arg arg = helper("{name[3:]}");
-            EXPECT_EQ(arg.as_variable(), "Tao");
-        }
+        EXPECT_EQ(helper("{name[2:3]}").as_variable(), " ");
 
-        {
-            format_arg arg = helper("{name[-3:]}");
-            EXPECT_EQ(arg.as_variable(), "Tao");
-        }
+        EXPECT_EQ(helper("{name[:]}").as_variable(), "Hu Tao");
 
-        {
-            format_arg arg = helper("{name[2:3]}");
-            EXPECT_EQ(arg.as_variable(), " ");
-        }
-
-        {
-            format_arg arg = helper("{name[:]}");
-            EXPECT_EQ(arg.as_variable(), "Hu Tao");
-        }
-
-        {
-            format_arg arg = helper("{name[:].length}");
-            EXPECT_EQ(arg.as_variable(), 6);
-        }
+        EXPECT_EQ(helper("{name[:].length}").as_variable(), 6);
     }
 }
 
-namespace test_interpreter
+namespace test_script_interpreter
 {
     template <typename... Args>
     auto run_script(std::string_view fmt, Args&&... args)
@@ -140,7 +95,7 @@ namespace test_interpreter
         auto fmt_args = PAPILIO_NS make_format_args(std::forward<Args>(args)...);
 
         format_parse_context parse_ctx(fmt, fmt_args);
-        parse_ctx.advance_to(parse_ctx.begin() + 2); // skip '{$'
+        parse_ctx.advance_to(parse_ctx.begin() + 2); // skip "{$"
 
         script::interpreter intp;
 
@@ -155,7 +110,7 @@ namespace test_interpreter
 TEST(interpreter, run)
 {
     using namespace papilio;
-    using namespace test_interpreter;
+    using namespace test_script_interpreter;
 
     {
         auto arg = run_script("{$ {val}: 'true'}", "val"_a = true);
