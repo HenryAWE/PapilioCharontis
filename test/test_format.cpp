91db51bb8f107306414059f1182edd4efe2dda06
AWE Henry <mouth11or12@outlook.com>
2023-12-08 23:57:39 +0800

    update unit test

--- test/test_format.cpp
+++ test/test_format.cpp
@@ -3,7 +3,6 @@
 
 
 static_assert(!papilio::formattable<std::monostate>);
-static_assert(!papilio::formattable<bool>);
 
 TEST(format, plain_text)
 {
@@ -26,7 +25,20 @@ TEST(format, script)
 {
     using namespace papilio;
 
+    EXPECT_EQ(PAPILIO_NS format("{$ {}: 'true'}", 1), "true");
+    EXPECT_EQ(PAPILIO_NS format("{$ !{}: 'false'}", 0), "false");
     EXPECT_EQ(PAPILIO_NS format("{$ {val}: 'true'}", "val"_a = 1), "true");
+
+    EXPECT_EQ(PAPILIO_NS format("{$ {} == {}: 'eq'}", 1, 1), "eq");
+    EXPECT_EQ(PAPILIO_NS format("{$ {} != {}: 'ne'}", 1, 2), "ne");
+
+    EXPECT_EQ(PAPILIO_NS format("{$ {} > {}: 'gt'}", 2, 1), "gt");
+    EXPECT_EQ(PAPILIO_NS format("{$ {} < {}: 'lt'}", 1, 2), "lt");
+
+    EXPECT_EQ(PAPILIO_NS format("{$ {} >= {}: 'ge'}", 2, 1), "ge");
+    EXPECT_EQ(PAPILIO_NS format("{$ {} <= {}: 'le'}", 1, 2), "le");
+    EXPECT_EQ(PAPILIO_NS format("{$ {} >= {}: 'ge'}", 1, 1), "ge");
+    EXPECT_EQ(PAPILIO_NS format("{$ {} <= {}: 'le'}", 1, 1), "le");
 }
 
 TEST(format, composite)
@@ -35,8 +47,26 @@ TEST(format, composite)
 
     EXPECT_EQ(PAPILIO_NS format("{} {}", 182375, 182376), "182375 182376");
 
-    EXPECT_EQ(PAPILIO_NS format("{0} warning{${0}>1:'s'}", 1), "1 warning");
-    EXPECT_EQ(PAPILIO_NS format("{0} warning{${0}>1:'s'}", 2), "2 warnings");
+    EXPECT_EQ(PAPILIO_NS format("{.length:*>4}", "hello"), "***5");
+    EXPECT_EQ(PAPILIO_NS format("length is {.length}", "hello"), "length is 5");
+
+    {
+        std::string_view fmt = "{0} warning{${0}>1:'s'}";
+
+        EXPECT_EQ(PAPILIO_NS format(fmt, 1), "1 warning");
+        EXPECT_EQ(PAPILIO_NS format(fmt, 2), "2 warnings");
+    }
+
+    {
+        std::string_view fmt =
+            "There"
+            " {${0} != 1: 'are' : 'is'} "
+            "{0}"
+            " apple{${0} != 1: 's'}";
+
+        EXPECT_EQ(PAPILIO_NS format(fmt, 1), "There is 1 apple");
+        EXPECT_EQ(PAPILIO_NS format(fmt, 2), "There are 2 apples");
+    }
 }
 
 int main(int argc, char* argv[])
