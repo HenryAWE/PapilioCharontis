00a78485726b3d7e79e417dbe819fd784149db75
AWE Henry <mouth11or12@outlook.com>
2024-06-10 14:04:33 +0800

    Update `std::tm` support
    1. Cross-platform consistency
    2. Fix warning in unit test

--- include/papilio/formatter/chrono.hpp
+++ include/papilio/formatter/chrono.hpp
@@ -4,6 +4,7 @@
 #pragma once
 
 #include <ctime>
+#include <span>
 #include <chrono>
 #include <iomanip>
 #include "../format.hpp"
@@ -39,22 +40,71 @@ public:
     auto format(const std::tm& val, Context& ctx) const
         -> typename Context::iterator
     {
-        std::basic_stringstream<CharT> ss;
-        ss.imbue(ctx.getloc());
-        // Use "%c\n" to simulate asctime() if the time format string is empty
-        const CharT* tm_fmt = m_fmt.empty() ?
-                                  PAPILIO_TSTRING(CharT, "%c\n") :
-                                  m_fmt.c_str();
-        ss << std::put_time(&val, tm_fmt);
+        if(m_fmt.empty())
+        {
+            CharT buf[24];
+            default_tm_fmt_impl(val, buf);
 
-        string_formatter<CharT> fmt;
-        fmt.set_data(m_data);
-        return fmt.format(std::move(ss).str(), ctx);
+            string_formatter<CharT> fmt;
+            fmt.set_data(m_data);
+            return fmt.format(std::basic_string_view<CharT>(buf, 24), ctx);
+        }
+        else
+        {
+            std::basic_stringstream<CharT> ss;
+            ss.imbue(ctx.getloc());
+            ss << std::put_time(&val, m_fmt.c_str());
+
+            string_formatter<CharT> fmt;
+            fmt.set_data(m_data);
+            return fmt.format(std::move(ss).str(), ctx);
+        }
     }
 
 private:
     simple_formatter_data m_data;
     utf::basic_string_container<CharT> m_fmt;
+
+    // Simulating `asctime()` but without the trailing newline
+    static void default_tm_fmt_impl(const std::tm& val, std::span<CharT, 24> buf)
+    {
+        constexpr CharT weekdays[7][3] = {
+            {'S', 'u', 'n'},
+            {'M', 'o', 'n'},
+            {'T', 'u', 'e'},
+            {'W', 'e', 'd'},
+            {'T', 'h', 'u'},
+            {'F', 'r', 'i'},
+            {'S', 'a', 't'}
+        };
+        constexpr CharT months[12][3] = {
+            {'J', 'a', 'n'},
+            {'F', 'e', 'b'},
+            {'M', 'a', 'r'},
+            {'A', 'p', 'r'},
+            {'M', 'a', 'y'},
+            {'J', 'u', 'n'},
+            {'J', 'u', 'l'},
+            {'A', 'u', 'g'},
+            {'S', 'e', 'p'},
+            {'O', 'c', 't'},
+            {'N', 'o', 'v'},
+            {'D', 'e', 'c'}
+        };
+
+        PAPILIO_NS format_to_n(
+            buf.data(),
+            buf.size(),
+            PAPILIO_TSTRING_VIEW(CharT, "{} {} {:2d} {:02d}:{:02d}:{:02d} {:4d}"),
+            std::basic_string_view<CharT>(weekdays[val.tm_wday], 3),
+            std::basic_string_view<CharT>(months[val.tm_mon], 3),
+            val.tm_mday,
+            val.tm_hour,
+            val.tm_min,
+            val.tm_sec,
+            val.tm_year + 1900
+        );
+    }
 };
 } // namespace papilio
 
--- test/test_format/chrono.cpp
+++ test/test_format/chrono.cpp
@@ -3,20 +3,35 @@
 #include <papilio/formatter/chrono.hpp>
 #include <papilio_test/setup.hpp>
 
+namespace test_format
+{
+static std::tm create_tm_epoch()
+{
+    // The Unix epoch (January 1, 1970)
+    const std::time_t t = 0;
+    std::tm result{};
+#ifdef PAPILIO_STDLIB_MSVC_STL
+    gmtime_s(&result, &t);
+#else
+    result = *std::gmtime(&t);
+#endif
+
+    return result;
+}
+} // namespace test_format
+
 TEST(formatter, tm)
 {
     using namespace papilio;
 
-    std::time_t t = 0;
-    std::tm* tm = std::gmtime(&t);
-
+    std::tm epoch = test_format::create_tm_epoch();
     static_assert(formattable<std::tm>);
     EXPECT_EQ(
-        PAPILIO_NS format("{}", *tm),
-        "Thu Jan  1 00:00:00 1970\n"
+        PAPILIO_NS format("{}", epoch),
+        "Thu Jan  1 00:00:00 1970"
     );
     EXPECT_EQ(
-        PAPILIO_NS format("{:=^32%c}", *tm),
+        PAPILIO_NS format("{:=^32}", epoch),
         "====Thu Jan  1 00:00:00 1970===="
     );
 }
