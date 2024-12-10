04d5db8e75c4754f59632cc07c9c13f7c9801ae6
AWE Henry <mouth11or12@outlook.com>
2024-12-10 19:32:32 +0800

    Update `<chrono>` support

--- include/papilio/formatter/chrono.hpp
+++ include/papilio/formatter/chrono.hpp
@@ -84,37 +84,65 @@ inline constexpr CharT month_names_short[12][3] = {
     {'D', 'e', 'c'}
 };
 
+template <typename CharT>
+inline constexpr std::basic_string_view<CharT> weekday_names_full[7] = {
+    PAPILIO_TSTRING_VIEW(CharT, "Sunday"),
+    PAPILIO_TSTRING_VIEW(CharT, "Monday"),
+    PAPILIO_TSTRING_VIEW(CharT, "Tuesday"),
+    PAPILIO_TSTRING_VIEW(CharT, "Wednesday"),
+    PAPILIO_TSTRING_VIEW(CharT, "Thursday"),
+    PAPILIO_TSTRING_VIEW(CharT, "Friday"),
+    PAPILIO_TSTRING_VIEW(CharT, "Saturday"),
+};
+
+template <typename CharT>
+inline constexpr std::basic_string_view<CharT> month_names_full[12] = {
+    PAPILIO_TSTRING_VIEW(CharT, "January"),
+    PAPILIO_TSTRING_VIEW(CharT, "February"),
+    PAPILIO_TSTRING_VIEW(CharT, "March"),
+    PAPILIO_TSTRING_VIEW(CharT, "April"),
+    PAPILIO_TSTRING_VIEW(CharT, "May"),
+    PAPILIO_TSTRING_VIEW(CharT, "June"),
+    PAPILIO_TSTRING_VIEW(CharT, "July"),
+    PAPILIO_TSTRING_VIEW(CharT, "August"),
+    PAPILIO_TSTRING_VIEW(CharT, "September"),
+    PAPILIO_TSTRING_VIEW(CharT, "October"),
+    PAPILIO_TSTRING_VIEW(CharT, "November"),
+    PAPILIO_TSTRING_VIEW(CharT, "December"),
+};
+
 #if defined(PAPILIO_COMPILER_CLANG)
 #    pragma clang diagnostic push
 #    pragma clang diagnostic ignored "-Wsign-conversion"
 #endif
 
-/**
- * @brief Similar to `std::asctime()` but without the trailing newline
- *
- * @param t Time value
- * @param buf (Uninitialized) buffer
- *
- */
-template <typename CharT>
-static void format_asctime(const std::tm& t, std::span<CharT, 24> buf)
+namespace detail
 {
-    int wday = std::clamp(t.tm_wday, 0, 6);
-    int mon = std::clamp(t.tm_mon, 0, 11);
+    /**
+    * @brief Similar to `std::asctime()` but without the trailing newline
+    *
+    * @param out Output iterator
+    * @param t Time value
+    */
+    template <typename CharT = char, typename OutputIt>
+    OutputIt put_asctime(OutputIt out, const std::tm& t)
+    {
+        int wday = std::clamp(t.tm_wday, 0, 6);
+        int mon = std::clamp(t.tm_mon, 0, 11);
 
-    PAPILIO_NS format_to_n(
-        buf.data(),
-        buf.size(),
-        PAPILIO_TSTRING_VIEW(CharT, "{} {} {:2d} {:02d}:{:02d}:{:02d} {:4d}"),
-        std::basic_string_view<CharT>(weekday_names_short<CharT>[wday], 3),
-        std::basic_string_view<CharT>(month_names_short<CharT>[mon], 3),
-        t.tm_mday,
-        t.tm_hour,
-        t.tm_min,
-        t.tm_sec,
-        t.tm_year + 1900
-    );
-}
+        return PAPILIO_NS format_to(
+            out,
+            PAPILIO_TSTRING_VIEW(CharT, "{} {} {:2d} {:02d}:{:02d}:{:02d} {:4d}"),
+            std::basic_string_view<CharT>(weekday_names_short<CharT>[wday], 3),
+            std::basic_string_view<CharT>(month_names_short<CharT>[mon], 3),
+            t.tm_mday,
+            t.tm_hour,
+            t.tm_min,
+            t.tm_sec,
+            t.tm_year + 1900
+        );
+    }
+} // namespace detail
 
 template <typename CharT>
 class formatter<std::tm, CharT>
@@ -138,12 +166,14 @@ public:
     {
         if(m_data.chrono_spec.empty())
         {
-            CharT buf[24];
-            PAPILIO_NS format_asctime<CharT>(val, buf);
+            small_vector<CharT, 24> buf;
+            PAPILIO_NS detail::put_asctime<CharT>(
+                std::back_inserter(buf), val
+            );
 
             string_formatter<CharT> fmt;
             fmt.set_data(m_data.basic);
-            return fmt.format(std::basic_string_view<CharT>(buf, 24), ctx);
+            return fmt.format(std::basic_string_view<CharT>(buf.data(), buf.size()), ctx);
         }
         else
         {
@@ -200,6 +230,26 @@ namespace detail
     inline std::tm to_tm(const std::chrono::year_month_day_last& date)
     {
         return to_tm(std::chrono::year_month_day(date));
+    }
+
+    inline std::tm to_tm(const std::chrono::year_month& date)
+    {
+        std::tm result = init_tm();
+
+        result.tm_year = static_cast<int>(date.year()) - 1900;
+        result.tm_mon = static_cast<unsigned int>(date.month()) - 1;
+
+        return result;
+    }
+
+    inline std::tm to_tm(const std::chrono::month_day& date)
+    {
+        std::tm result = init_tm();
+
+        result.tm_mon = static_cast<unsigned int>(date.month()) - 1;
+        result.tm_mday = static_cast<unsigned int>(date.day());
+
+        return result;
     }
 
     inline std::tm to_tm(const std::chrono::year_month_day& date, std::chrono::weekday weekday)
@@ -329,6 +379,11 @@ namespace detail
         return result;
     }
 
+    inline std::tm to_tm(const std::chrono::sys_info&)
+    {
+        return init_tm();
+    }
+
     template <typename CharT>
     void format_century(std::basic_stringstream<CharT>& ss, int year)
     {
@@ -375,7 +430,7 @@ namespace detail
     }
 
     template <typename CharT, typename OutputIt>
-    OutputIt put_weekday_by_name(OutputIt out, const std::chrono::weekday& wd)
+    OutputIt put_weekday_by_name(OutputIt out, const std::chrono::weekday& wd, bool fullname)
     {
         if(!wd.ok()) [[unlikely]]
         {
@@ -389,12 +444,18 @@ namespace detail
         {
             unsigned int wday = wd.c_encoding();
             PAPILIO_ASSERT(wday < 7);
-            return std::copy_n(weekday_names_short<CharT>[wday], 3, out);
+            if(fullname)
+            {
+                std::basic_string_view<CharT> sv = weekday_names_full<CharT>[wday];
+                return std::copy(sv.begin(), sv.end(), out);
+            }
+            else
+                return std::copy_n(weekday_names_short<CharT>[wday], 3, out);
         }
     }
 
     template <typename CharT, typename OutputIt>
-    OutputIt put_month_by_name(OutputIt out, const std::chrono::month& m)
+    OutputIt put_month_by_name(OutputIt out, const std::chrono::month& m, bool fullname)
     {
         if(!m.ok()) [[unlikely]]
         {
@@ -408,7 +469,13 @@ namespace detail
         {
             unsigned int mon = static_cast<unsigned int>(m) - 1;
             PAPILIO_ASSERT(mon < 12);
-            return std::copy_n(month_names_short<CharT>[mon], 3, out);
+            if(fullname)
+            {
+                std::basic_string_view<CharT> sv = month_names_full<CharT>[mon];
+                return std::copy(sv.begin(), sv.end(), out);
+            }
+            else
+                return std::copy_n(month_names_short<CharT>[mon], 3, out);
         }
     }
 
@@ -491,6 +558,155 @@ namespace detail
             out
         );
     }
+
+    template <typename ChronoType>
+    consteval bool has_fractional_width()
+    {
+        using std::is_same_v;
+        using namespace std::chrono;
+
+        if constexpr(is_specialization_of_v<ChronoType, time_point>)
+            return has_fractional_width<typename ChronoType::duration>();
+        if constexpr(is_specialization_of_v<ChronoType, duration>)
+            return has_fractional_width<hh_mm_ss<ChronoType>>();
+        else if constexpr(is_specialization_of_v<ChronoType, hh_mm_ss>)
+            return ChronoType::fractional_width;
+        else
+            return false;
+    }
+
+    template <typename CharT, typename OutputIt>
+    OutputIt put_decimal_point(OutputIt out)
+    {
+        *out = CharT('.');
+        ++out;
+        return out;
+    }
+
+    template <typename CharT, typename OutputIt, typename Rep, typename Period>
+    OutputIt put_subseconds(OutputIt out, const std::chrono::duration<Rep, Period>& val)
+    {
+        out = put_decimal_point<CharT>(out);
+
+        using namespace std::chrono;
+        using duration_type = duration<Rep, Period>;
+
+        auto abs_val = abs(val);
+        auto frac = abs_val - duration_cast<std::chrono::seconds>(abs_val);
+
+        using hh_mm_ss_type = hh_mm_ss<duration_type>;
+        if constexpr(treat_as_floating_point_v<Rep>)
+        {
+            return PAPILIO_NS format_to(
+                out,
+                PAPILIO_TSTRING_VIEW(CharT, "{:0{}.0f}"),
+                duration_cast<typename hh_mm_ss_type::precision>(frac).count(),
+                hh_mm_ss_type::fractional_width
+            );
+        }
+        else
+        {
+            return PAPILIO_NS format_to(
+                out,
+                PAPILIO_TSTRING_VIEW(CharT, "{:0{}}"),
+                duration_cast<typename hh_mm_ss_type::precision>(frac).count(),
+                hh_mm_ss_type::fractional_width
+            );
+        }
+    }
+
+    template <typename CharT, typename OutputIt, typename Clock, typename Duration>
+    OutputIt put_subseconds(OutputIt out, const std::chrono::time_point<Clock, Duration>& val)
+    {
+        return put_subseconds<CharT>(std::move(out), val.time_since_epoch());
+    }
+
+    template <typename CharT, typename OutputIt, typename Duration>
+    OutputIt put_subseconds(OutputIt out, const std::chrono::hh_mm_ss<Duration>& val)
+    {
+        out = put_decimal_point<CharT>(out);
+
+        if constexpr(std::chrono::treat_as_floating_point_v<typename Duration::rep>)
+        {
+            return PAPILIO_NS format_to(
+                out,
+                PAPILIO_TSTRING_VIEW(CharT, "{:0{}.0f}"),
+                val.subseconds().count(),
+                val.fractional_width
+            );
+        }
+        else
+        {
+            return PAPILIO_NS format_to(
+                out,
+                PAPILIO_TSTRING_VIEW(CharT, "{:0{}}"),
+                val.subseconds().count(),
+                val.fractional_width
+            );
+        }
+    }
+
+    /**
+     * @brief Time zone information needed for formatting
+     */
+    struct timezone_info
+    {
+        std::string abbrev;
+        std::chrono::seconds offset;
+    };
+
+    template <typename ChronoType>
+    timezone_info get_timezone_info(const ChronoType& val)
+    {
+        using std::is_same_v;
+        if constexpr(is_same_v<ChronoType, std::chrono::sys_info>)
+            return {val.abbrev, val.offset};
+
+        return {"UTC", std::chrono::seconds(0)};
+    }
+
+    template <typename CharT, typename OutputIt>
+    OutputIt put_timezone_abbrev(OutputIt out, const timezone_info& tz)
+    {
+        // The abbreviation always consists of char, so use copy() to convert
+        return std::copy(tz.abbrev.begin(), tz.abbrev.end(), out);
+    }
+
+    template <typename CharT, typename OutputIt>
+    OutputIt put_timezone_offset(OutputIt out, std::chrono::seconds offset, bool alt_fmt)
+    {
+        if(offset < std::chrono::seconds(0))
+        {
+            *out = CharT('-');
+            ++out;
+            offset = -offset;
+        }
+        else
+        {
+            *out = CharT('+');
+            ++out;
+        }
+
+        std::chrono::hh_mm_ss hms(offset);
+        if(alt_fmt)
+        {
+            return PAPILIO_NS format_to(
+                out,
+                PAPILIO_TSTRING_VIEW(CharT, "{:02}:{:02}"),
+                hms.hours().count(),
+                hms.minutes().count()
+            );
+        }
+        else
+        {
+            return PAPILIO_NS format_to(
+                out,
+                PAPILIO_TSTRING_VIEW(CharT, "{:02}{:02}"),
+                hms.hours().count(),
+                hms.minutes().count()
+            );
+        }
+    }
 } // namespace detail
 
 template <typename ChronoType, typename CharT>
@@ -523,17 +739,17 @@ public:
                 ctx
             );
         }
-        if(m_data.basic.use_locale)
+        else if(!m_data.basic.use_locale)
         {
             return fmt.format(
-                to_str_by_facet(ctx.getloc(), val, m_data.chrono_spec),
+                to_str(val, m_data.chrono_spec),
                 ctx
             );
         }
         else
         {
             return fmt.format(
-                to_str(val, m_data.chrono_spec),
+                to_str_by_facet(ctx.getloc(), val, m_data.chrono_spec),
                 ctx
             );
         }
@@ -570,7 +786,8 @@ private:
         {
             detail::put_month_by_name<CharT>(
                 std::back_inserter(result),
-                val
+                val,
+                false
             );
         }
         else if constexpr(is_same_v<ChronoType, day>)
@@ -588,11 +805,28 @@ private:
         {
             result = to_str(val, PAPILIO_TSTRING_VIEW(CharT, "%F"));
         }
+        else if constexpr(is_same_v<ChronoType, year_month>)
+        {
+            result = PAPILIO_NS format(
+                PAPILIO_TSTRING_VIEW(CharT, "{}/{}"),
+                val.year(),
+                val.month()
+            );
+        }
+        else if constexpr(is_same_v<ChronoType, month_day>)
+        {
+            result = PAPILIO_NS format(
+                PAPILIO_TSTRING_VIEW(CharT, "{}/{}"),
+                val.month(),
+                val.day()
+            );
+        }
         else if constexpr(is_same_v<ChronoType, weekday>)
         {
             detail::put_weekday_by_name<CharT>(
                 std::back_inserter(result),
-                val
+                val,
+                false
             );
         }
         else if constexpr(is_same_v<ChronoType, weekday_indexed>)
@@ -610,6 +844,17 @@ private:
                 val.weekday()
             );
         }
+        else if constexpr(is_same_v<ChronoType, sys_info>)
+        {
+            result = PAPILIO_NS format(
+                PAPILIO_TSTRING_VIEW(CharT, "({}, {}, {}, {}, {})"),
+                val.begin,
+                val.end,
+                val.offset,
+                val.save,
+                val.abbrev
+            );
+        }
 
         return result;
     }
@@ -617,6 +862,7 @@ private:
     static std::basic_string<CharT> to_str(const ChronoType& val, utf::basic_string_ref<CharT> spec)
     {
         std::tm t = detail::to_tm(val);
+        detail::timezone_info tz = PAPILIO_NS detail::get_timezone_info(val);
 
         static constexpr bool has_count = requires() {
             val.count();
@@ -653,10 +899,6 @@ private:
                 ss << static_cast<CharT>('%');
                 continue;
 
-            case U'O':
-            case U'E':
-                throw_bad_format("locale is not supported without 'L'");
-
             case U'C':
                 detail::format_century(ss, t.tm_year + 1900);
                 continue;
@@ -676,9 +918,11 @@ private:
                 );
                 continue;
             case U'b':
+            case U'B':
                 detail::put_month_by_name<CharT>(
                     std::ostreambuf_iterator<CharT>(ss),
-                    std::chrono::month(t.tm_mon + 1)
+                    std::chrono::month(t.tm_mon + 1),
+                    ch32 == U'B'
                 );
                 continue;
 
@@ -705,9 +949,20 @@ private:
                 detail::format_weekday(ss, t.tm_wday, false);
                 continue;
             case U'a':
+            case U'A':
                 detail::put_weekday_by_name<CharT>(
                     std::ostreambuf_iterator<CharT>(ss),
-                    std::chrono::weekday(t.tm_wday)
+                    std::chrono::weekday(t.tm_wday),
+                    ch32 == U'A'
+                );
+                continue;
+
+                // Day of the year
+            case 'j':
+                PAPILIO_NS format_to(
+                    std::ostreambuf_iterator<CharT>(ss),
+                    PAPILIO_TSTRING_VIEW(CharT, "{:03d}"),
+                    t.tm_yday + 1
                 );
                 continue;
 
@@ -740,6 +995,27 @@ private:
                     PAPILIO_TSTRING_VIEW(CharT, "{:02d}"),
                     t.tm_sec
                 );
+                if constexpr(detail::has_fractional_width<ChronoType>())
+                {
+                    detail::put_subseconds<CharT>(
+                        std::ostreambuf_iterator<CharT>(ss),
+                        val
+                    );
+                }
+                continue;
+
+            case 'z':
+                PAPILIO_NS detail::put_timezone_offset<CharT>(
+                    std::ostreambuf_iterator<CharT>(ss),
+                    tz.offset,
+                    false
+                );
+                continue;
+            case 'Z':
+                PAPILIO_NS detail::put_timezone_abbrev<CharT>(
+                    std::ostreambuf_iterator<CharT>(ss),
+                    tz
+                );
                 continue;
 
             case U'R':
@@ -758,6 +1034,13 @@ private:
                     t.tm_min,
                     t.tm_sec
                 );
+                if constexpr(detail::has_fractional_width<ChronoType>())
+                {
+                    detail::put_subseconds<CharT>(
+                        std::ostreambuf_iterator<CharT>(ss),
+                        val
+                    );
+                }
                 continue;
 
             case U'D': // Equivalent to %m/%d/%y
@@ -779,6 +1062,12 @@ private:
                 );
                 continue;
 
+            case U'c':
+                PAPILIO_NS detail::put_asctime(
+                    std::ostreambuf_iterator<CharT>(ss), t
+                );
+                continue;
+
             case U'q':
             case U'Q':
                 if constexpr(has_count)
@@ -790,6 +1079,27 @@ private:
                 else
                 {
                     throw_bad_format("count() is not supported for this type");
+                }
+                continue;
+
+            case U'O':
+            case U'E':
+                if(it == sentinel)
+                {
+                    throw_bad_format("invalid format for locale");
+                }
+                else if(char32_t loc_ch = *std::next(it); loc_ch == U'z')
+                {
+                    PAPILIO_NS detail::put_timezone_offset<CharT>(
+                        std::ostreambuf_iterator<CharT>(ss),
+                        tz.offset,
+                        true
+                    );
+                    ++it;
+                }
+                else
+                {
+                    throw_bad_format("locale is not supported without 'L'");
                 }
                 continue;
 
@@ -855,6 +1165,16 @@ class formatter<std::chrono::duration<Rep, Period>, CharT> :
 {};
 
 template <typename CharT>
+class formatter<std::chrono::year_month, CharT> :
+    public chrono_formatter<std::chrono::year_month, CharT>
+{};
+
+template <typename CharT>
+class formatter<std::chrono::month_day, CharT> :
+    public chrono_formatter<std::chrono::month_day, CharT>
+{};
+
+template <typename CharT>
 class formatter<std::chrono::year_month_day, CharT> :
     public chrono_formatter<std::chrono::year_month_day, CharT>
 {};
@@ -897,6 +1217,11 @@ class formatter<std::chrono::weekday_last, CharT> :
 template <typename Duration, typename CharT>
 class formatter<std::chrono::hh_mm_ss<Duration>, CharT> :
     public chrono_formatter<std::chrono::hh_mm_ss<Duration>, CharT>
+{};
+
+template <typename CharT>
+class formatter<std::chrono::sys_info, CharT> :
+    public chrono_formatter<std::chrono::sys_info, CharT>
 {};
 } // namespace papilio
 
--- test/test_format/chrono.cpp
+++ test/test_format/chrono.cpp
@@ -1,4 +1,7 @@
 #include <gtest/gtest.h>
+#if __has_include(<format>)
+#    include <format> // Test ADL-proof
+#endif
 #include <papilio/papilio.hpp>
 #include <papilio/formatter/chrono.hpp>
 #include <papilio_test/setup.hpp>
@@ -56,9 +59,11 @@ TEST(formatter, chrono)
         EXPECT_EQ(PAPILIO_NS format("{:%m}", std::chrono::January), "01");
         EXPECT_EQ(PAPILIO_NS format("{:%m}", std::chrono::December), "12");
         EXPECT_EQ(PAPILIO_NS format("{:%b}", std::chrono::January), "Jan");
+        EXPECT_EQ(PAPILIO_NS format("{:%B}", std::chrono::January), "January");
         EXPECT_EQ(PAPILIO_NS format("{:%b}", std::chrono::December), "Dec");
+        EXPECT_EQ(PAPILIO_NS format("{:%B}", std::chrono::December), "December");
         EXPECT_EQ(PAPILIO_NS format("{:%b}", std::chrono::month(13)), "month(13)");
-        EXPECT_EQ(PAPILIO_NS format("{:%b}", std::chrono::month(13)), "month(13)");
+        EXPECT_EQ(PAPILIO_NS format("{:%B}", std::chrono::month(13)), "month(13)");
         EXPECT_EQ(PAPILIO_NS format("{}", std::chrono::January), "Jan");
         EXPECT_EQ(PAPILIO_NS format("{}", std::chrono::December), "Dec");
         EXPECT_EQ(PAPILIO_NS format("{}", std::chrono::month(13)), "month(13)");
@@ -93,6 +98,15 @@ TEST(formatter, chrono)
         EXPECT_EQ(PAPILIO_NS format("{:%T}", hms), PAPILIO_NS format("{:%H:%M:%S}", hms));
         EXPECT_EQ(PAPILIO_NS format("{}", hms), "00:01:05");
         EXPECT_EQ(PAPILIO_NS format("{}", hms), PAPILIO_NS format("{:%T}", hms));
+
+        {
+            std::chrono::hh_mm_ss<std::chrono::milliseconds> hms_ms{100ms};
+            SCOPED_TRACE("fractional_width = " + std::to_string(hms_ms.fractional_width));
+
+            EXPECT_EQ(PAPILIO_NS format("{:%S}", hms_ms), "00.100");
+            EXPECT_EQ(PAPILIO_NS format("{:%T}", hms_ms), "00:00:00.100");
+            EXPECT_EQ(PAPILIO_NS format("{:%T}", hms_ms), PAPILIO_NS format("{:%H:%M:%S}", hms_ms));
+        }
     }
 
     // Day of the week
@@ -101,11 +115,17 @@ TEST(formatter, chrono)
         EXPECT_EQ(PAPILIO_NS format("{:%w}", std::chrono::Sunday), "0");
         EXPECT_EQ(PAPILIO_NS format("{:%u}", std::chrono::Sunday), "7");
         EXPECT_EQ(PAPILIO_NS format("{:%a}", std::chrono::Sunday), "Sun");
+        EXPECT_EQ(PAPILIO_NS format("{:%A}", std::chrono::Sunday), "Sunday");
         EXPECT_EQ(PAPILIO_NS format("{}", std::chrono::Sunday), "Sun");
         EXPECT_EQ(PAPILIO_NS format("{:%w}", std::chrono::Monday), "1");
         EXPECT_EQ(PAPILIO_NS format("{:%u}", std::chrono::Monday), "1");
         EXPECT_EQ(PAPILIO_NS format("{:%a}", std::chrono::Monday), "Mon");
+        EXPECT_EQ(PAPILIO_NS format("{:%A}", std::chrono::Monday), "Monday");
         EXPECT_EQ(PAPILIO_NS format("{}", std::chrono::Monday), "Mon");
+
+        EXPECT_EQ(PAPILIO_NS format("{}", std::chrono::weekday(8)), "weekday(8)");
+        EXPECT_EQ(PAPILIO_NS format("{:%a}", std::chrono::weekday(8)), "weekday(8)");
+        EXPECT_EQ(PAPILIO_NS format("{:%A}", std::chrono::weekday(8)), "weekday(8)");
 
         static_assert(formattable<decltype(std::chrono::Sunday[1])>);
         EXPECT_EQ(PAPILIO_NS format("{:%w}", std::chrono::Sunday[1]), "0");
@@ -131,10 +151,18 @@ TEST(formatter, chrono)
 
     // %Q and %q
     {
+        EXPECT_EQ(PAPILIO_NS format("{}", 1ns), "1ns");
+        EXPECT_EQ(PAPILIO_NS format("{}", 1us), "1us");
+        EXPECT_EQ(PAPILIO_NS format("{}", 1ms), "1ms");
+        EXPECT_EQ(PAPILIO_NS format("{}", 1s), "1s");
+        EXPECT_EQ(PAPILIO_NS format("{}", 1min), "1min");
+        EXPECT_EQ(PAPILIO_NS format("{}", 1h), "1h");
+
         auto d0 = std::chrono::duration_cast<std::chrono::seconds>(
             std::chrono::sys_days(2023y / 11 / 8) - std::chrono::sys_days(2023y / 11 / 8)
         );
 
+        static_assert(formattable<decltype(d0)>);
         EXPECT_EQ(PAPILIO_NS format("{:%Q}", d0), "0");
         EXPECT_EQ(PAPILIO_NS format("{:%q}", d0), "0s");
         EXPECT_EQ(PAPILIO_NS format("{}", d0), "0s");
@@ -143,6 +171,7 @@ TEST(formatter, chrono)
             std::chrono::sys_days(2023y / 11 / 8) - std::chrono::sys_days(2023y / 11 / 1)
         );
 
+        static_assert(formattable<decltype(d7)>);
         EXPECT_EQ(PAPILIO_NS format("{:%Q}", d7), "7");
         EXPECT_EQ(PAPILIO_NS format("{:%q}", d7), "7d");
         EXPECT_EQ(PAPILIO_NS format("{}", d7), "7d");
@@ -152,15 +181,31 @@ TEST(formatter, chrono)
     {
         auto date = 2023y / 11 / 8;
         static_assert(formattable<decltype(date)>);
+        static_assert(formattable<decltype(2023y / 11)>);
+        static_assert(formattable<decltype(11 / 8d)>);
 
         EXPECT_EQ(PAPILIO_NS format("{:%Y}", date), "2023");
         EXPECT_EQ(PAPILIO_NS format("{:%y}", date), "23");
+        EXPECT_EQ(PAPILIO_NS format("{:%Y-%m}", 2023y / 11), "2023-11");
+        EXPECT_EQ(PAPILIO_NS format("{}", 2023y / 11), "2023/Nov");
+        EXPECT_EQ(PAPILIO_NS format("{:%m-%d}", 11 / 8d), "11-08");
+        EXPECT_EQ(PAPILIO_NS format("{}", 11 / 8d), "Nov/08");
 
         EXPECT_EQ(PAPILIO_NS format("{:%D}", date), "11/08/23");
         EXPECT_EQ(PAPILIO_NS format("{:%D}", date), PAPILIO_NS format("{:%m/%d/%y}", date));
         EXPECT_EQ(PAPILIO_NS format("{:%F}", date), "2023-11-08");
         EXPECT_EQ(PAPILIO_NS format("{:%F}", date), PAPILIO_NS format("{:%Y-%m-%d}", date));
         EXPECT_EQ(PAPILIO_NS format("{}", date), PAPILIO_NS format("{:%F}", date));
+
+        EXPECT_EQ(PAPILIO_NS format("{:%c}", date), "Wed Nov  8 00:00:00 2023");
+    }
+
+    // %j
+    {
+        EXPECT_EQ(PAPILIO_NS format("{:%j}", 2023y / 1 / 1), "001");
+        EXPECT_EQ(PAPILIO_NS format("{:%j}", 2023y / 12 / 31), "365");
+        EXPECT_EQ(PAPILIO_NS format("{:%j}", 2024y / 1 / 1), "001");
+        EXPECT_EQ(PAPILIO_NS format("{:%j}", 2024y / 12 / 31), "366");
     }
 
     {
@@ -168,12 +213,25 @@ TEST(formatter, chrono)
         system_clock::time_point t = std::chrono::sys_days(date);
         static_assert(formattable<system_clock::time_point>);
 
-        EXPECT_EQ(PAPILIO_NS format("{:%F %T}", t), "2023-11-08 00:00:00");
-        EXPECT_EQ(PAPILIO_NS format("{}", t), "2023-11-08 00:00:00");
+        EXPECT_EQ(PAPILIO_NS format("{:%F}", t), "2023-11-08");
+        EXPECT_EQ(PAPILIO_NS format("{:%Z}", t), "UTC");
+        EXPECT_EQ(PAPILIO_NS format("{:%z}", t), "+0000");
+        EXPECT_EQ(PAPILIO_NS format("{:%Ez}", t), "+00:00");
+        EXPECT_EQ(PAPILIO_NS format("{:%Oz}", t), "+00:00");
         EXPECT_EQ(PAPILIO_NS format("{}", t), PAPILIO_NS format("{:%F %T}", t));
 
         // Print platform-dependent result for visual check
-        PAPILIO_NS println(std::cerr, "now(): {}", system_clock::now());
+        auto sys_now = std::chrono::system_clock::now();
+        PAPILIO_NS println(
+            std::cerr,
+            "now(): {0}\n"
+            "fractional_width = {1}\n"
+            "timezone: {2:%z %Z}\n"
+            "sys_info: {2}",
+            sys_now,
+            std::chrono::hh_mm_ss<system_clock::duration>::fractional_width,
+            std::chrono::current_zone()->get_info(sys_now)
+        );
     }
 
     // Plain text and special characters
